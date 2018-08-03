2430941f4b710b1cee138332a31675a217cf0ac5
jodnddus <lockhost23@gmail.com>
2018-08-03 22:58:31 +0900

    typescript is superset of javascript

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "jodnddus",
+    age = 18,
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age, gender);
+
+export{};
+
