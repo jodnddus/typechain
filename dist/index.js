2f495fa7ce5491c8e710a3b89587e7d4353fb995
jodnddus <lockhost23@gmail.com>
2018-08-04 00:20:20 +0900

    I learning types in Typescript!

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
-console.log("msg");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Helloooooooooooooo ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("jodnddus", 18, "male"));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("msg");
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Helloooooooooooooo ${name}, you are ${age}, you are a ${gender}`;
+}
+
+console.log(sayHi("jodnddus", 18, "male"));
+
+export{};
