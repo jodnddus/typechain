c4fc4855825dc25e695c0624905e7310d70d81be
jodnddus <lockhost23@gmail.com>
2018-08-05 23:56:07 +0900

    createBlock method compleate!

--- dist/index.js
+++ dist/index.js
@@ -16,4 +16,13 @@ let blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimeStamp = () => Math.round(new Data().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previosBlock = getLatestBlock();
+    const newIndex = previosBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("Hello"), createNewBlock("bye bye!"));
 //# sourceMappingURL=index.js.map
